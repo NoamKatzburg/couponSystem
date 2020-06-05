bb323159aeed3b5b132341d58fb461b03425aac5
Noam Katzburg <noamkatzburg@gmail.com>
2020-06-05 21:11:01 +0300

    finished dbdao for customers

--- src/noam/beans/Customer.java
+++ src/noam/beans/Customer.java
@@ -20,6 +20,15 @@ public class Customer {
 		this.password = password;
 		this.coupons = coupons;
 	}
+	
+	public Customer(int id, String firstName, String lastName, String email, String password) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+	}
+	
 
 	public int getId() {
 		return id;
--- src/noam/dbdao/CustomersDBDAO.java
+++ src/noam/dbdao/CustomersDBDAO.java
@@ -4,7 +4,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.List;
 
 import noam.beans.Company;
 import noam.beans.Customer;
@@ -12,7 +14,7 @@ import noam.dao.CustomersDAO;
 import noam.db.ConnectionPool;
 
 public class CustomersDBDAO implements CustomersDAO {
-	
+
 	private Connection connection;
 
 	public boolean isCustomerExist(String email, String password) {
@@ -20,7 +22,7 @@ public class CustomersDBDAO implements CustomersDAO {
 
 		try {
 			connection = ConnectionPool.getInstance().getConnection();
-			String sql = "SELECT * FROM `coupon_system`.`customers` WHERE  email= ? AND password=?";
+			String sql = "SELECT * FROM `coupon_system`.`customers` WHERE  `email`= ? AND `password`=?";
 
 			PreparedStatement statement = connection.prepareStatement(sql);
 			statement.setString(1, email);
@@ -31,7 +33,7 @@ public class CustomersDBDAO implements CustomersDAO {
 				return true;
 			}
 			resultSet.close();
-			
+
 		} catch (InterruptedException | SQLException e) {
 			e.getMessage();
 		} finally {
@@ -39,32 +41,128 @@ public class CustomersDBDAO implements CustomersDAO {
 		}
 
 		return false;
-		
+
 	}
 
 	public void addCustomer(Customer customer) {
-		// TODO Auto-generated method stub
+		connection = null;
+		try {
+			connection = ConnectionPool.getInstance().getConnection();
+
+			String sql = "INSERT INTO `coupon_system`.`customers` (`first_name`, `last_name`, `email`, `password`) VALUES (?, ?, ?, ?);\r\n"
+					+ "";
+			PreparedStatement statement = connection.prepareStatement(sql);
+			statement.setString(1, customer.getFirstName());
+			statement.setString(2, customer.getLastName());
+			statement.setString(3, customer.getEmail());
+			statement.setString(4, customer.getPassword());
+			statement.executeUpdate();
+		} catch (Exception e) {
+			e.getMessage();
+		} finally {
+			ConnectionPool.getInstance().returnConnection(connection);
+		}
 
 	}
 
 	public void updateCustomer(Customer customer) {
-		// TODO Auto-generated method stub
+		connection = null;
+		try {
+			connection = ConnectionPool.getInstance().getConnection();
+
+			String sql = "UPDATE `coupon_system`.`customers` SET `first_name` = ?, `last_name` = ?, `email` = ?, `password` = ? WHERE (`id` = ?');\r\n";
+			PreparedStatement statement = connection.prepareStatement(sql);
+			statement.setString(1, customer.getFirstName());
+			statement.setString(2, customer.getLastName());
+			statement.setString(3, customer.getEmail());
+			statement.setString(4, customer.getPassword());
+			statement.setInt(5, customer.getId());
+
+			statement.executeUpdate();
+		} catch (Exception e) {
+			e.getMessage();
+		} finally {
+			ConnectionPool.getInstance().returnConnection(connection);
+		}
 
 	}
 
 	public void deleteCustomer(int customerID) {
-		// TODO Auto-generated method stub
+		connection = null;
+		try {
+			connection = ConnectionPool.getInstance().getConnection();
+
+			String sql = "DELETE FROM `coupon_system`.`customers` WHERE (`id` = ?);";
+			PreparedStatement statement = connection.prepareStatement(sql);
+			statement.setInt(1, customerID);
+
+			statement.executeUpdate();
+		} catch (Exception e) {
+			e.getMessage();
+		} finally {
+			ConnectionPool.getInstance().returnConnection(connection);
+		}
 
 	}
 
 	public ArrayList<Customer> getAllCustomers() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Customer> customers = new ArrayList<Customer>();
+
+		connection = null;
+		try {
+			connection = ConnectionPool.getInstance().getConnection();
+
+			String sql = "SELECT * FROM `coupon_system`.`customers`;";
+
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery(sql);
+
+			while (resultSet.next()) {
+				int id = resultSet.getInt(1);
+				String firstName = resultSet.getString(2);
+				String lastName = resultSet.getString(3);
+				String email = resultSet.getString(4);
+				String password = resultSet.getString(5);
+
+				customers.add(new Customer(id, firstName, lastName, email, password));
+			}
+
+		} catch (Exception e) {
+			e.getMessage();
+		} finally {
+			ConnectionPool.getInstance().returnConnection(connection);
+		}
+		return (ArrayList<Customer>) customers;
 	}
 
 	public Customer getOneCustomer(int customerID) {
-		// TODO Auto-generated method stub
-		return null;
+		Customer c1 = null;
+		connection = null;
+		try {
+			connection = ConnectionPool.getInstance().getConnection();
+
+			String sql = "SELECT * FROM `coupon_system`.`customers` WHERE (`id`= ?);";
+
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery(sql);
+
+			while (resultSet.next()) {
+				int id = resultSet.getInt(1);
+				String firstName = resultSet.getString(2);
+				String lastName = resultSet.getString(3);
+				String email = resultSet.getString(4);
+				String password = resultSet.getString(5);
+
+				c1 = new Customer(id, firstName, lastName, email, password);
+			}
+
+		} catch (Exception e) {
+			e.getMessage();
+		} finally {
+			ConnectionPool.getInstance().returnConnection(connection);
+		}
+		return c1;
+
 	}
 
 }
