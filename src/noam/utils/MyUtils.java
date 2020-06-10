a02181a3474f819b244565e24fbb5d059781811d
Noam Katzburg <noamkatzburg@gmail.com>
2020-06-10 14:32:28 +0300

    created utils package

---
+++ src/noam/utils/MyUtils.java
@@ -0,0 +1,9 @@
+package noam.utils;
+
+public class MyUtils {
+
+	public static java.sql.Date convertUtilToSql(java.util.Date uDate) {
+        java.sql.Date sDate = new java.sql.Date(uDate.getTime());
+        return sDate;
+    }
+}
