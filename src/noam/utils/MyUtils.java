b8f76fac3e1ca2eaa8d5d8b3e7d031873fc7d3e4
Noam Katzburg <noamkatzburg@gmail.com>
2020-06-10 22:29:35 +0300

    created sql to util converter method

--- src/noam/utils/MyUtils.java
+++ src/noam/utils/MyUtils.java
@@ -1,9 +1,18 @@
 package noam.utils;
 
+import noam.beans.Category;
+
 public class MyUtils {
 
 	public static java.sql.Date convertUtilToSql(java.util.Date uDate) {
         java.sql.Date sDate = new java.sql.Date(uDate.getTime());
         return sDate;
     }
+	
+	public static java.util.Date convertSqlToUtil(java.sql.Date sDate) {
+        java.util.Date uDate = new java.util.Date(sDate.getTime());
+        return uDate;
+    }
+	
+	
 }
