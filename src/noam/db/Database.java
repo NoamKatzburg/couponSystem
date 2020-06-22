66346d2e2ec8e8fc2954c4a9e1af6e030defc6cf
Noam Katzburg <noamkatzburg@gmail.com>
2020-06-22 22:29:28 +0300

    deleted create data method

--- src/noam/db/Database.java
+++ src/noam/db/Database.java
@@ -24,12 +24,34 @@ public class Database {
 	}
 
 	public static void initDB() throws SQLException, InterruptedException {
+		dropSchema();
 		createSchema();
 		createTableCompanies();
 		createTableCategories();
 		createTableCustomers();
 		createTableCoupons();
 		createTableCustomerVsCoupons();
+	}
+
+	public static void dropSchema() throws SQLException, InterruptedException {
+
+		try {
+			// STEP 2
+			connection = ConnectionPool.getInstance().getConnection();
+
+			String sql = "DROP DATABASE `coupon_system`;";
+
+			// STEP 3
+			PreparedStatement statment = connection.prepareStatement(sql);
+			statment.executeUpdate();
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+			// STEP 5
+			ConnectionPool.getInstance().returnConnection(connection);
+			connection = null;
+		}
 	}
 
 	public static void createSchema() throws SQLException, InterruptedException {
@@ -110,8 +132,7 @@ public class Database {
 			connection = ConnectionPool.getInstance().getConnection();
 
 			String sql = "CREATE TABLE `coupon_system`.`categories` (\r\n" + "  `id` INT NOT NULL AUTO_INCREMENT,\r\n"
-					+ "  `name` VARCHAR(45) NOT NULL,\r\n" + "  PRIMARY KEY (`id`));"
-					+ "INSERT INTO `coupon_system`.`categories` (`name`) VALUES ('Food'); INSERT INTO `coupon_system`.`categories` (`name`) VALUES ('Electricty');INSERT INTO `coupon_system`.`categories` (`name`) VALUES ('Restaurant');INSERT INTO `coupon_system`.`categories` (`name`) VALUES ('Vacation');";
+					+ "  `name` VARCHAR(45) NOT NULL,\r\n" + "  PRIMARY KEY (`id`));";
 
 			// STEP 3
 			PreparedStatement statment = connection.prepareStatement(sql);
@@ -189,70 +210,6 @@ public class Database {
 
 	}
 
-	public static void createData() {
-		compData();
-		custData();
-		coupData();
-	}
-
-	public static void compData() {
-		connection = null;
-		try {
-			connection = ConnectionPool.getInstance().getConnection();
-
-			String sql = "INSERT INTO `coupon_system`.`companies` (`name`, `email`, `password`) VALUES ('market', 'market@food.com', '1234');\r\n"
-					+ "INSERT INTO `coupon_system`.`companies` (`name`, `email`, `password`) VALUES ('electric', 'elec@tricuty.com', '1234');\r\n"
-					+ "INSERT INTO `coupon_system`.`companies` (`name`, `email`, `password`) VALUES ('McDonalds', 'mc@donalds.com', '1234');\r\n"
-					+ "INSERT INTO `coupon_system`.`companies` (`name`, `email`, `password`) VALUES ('spa', 'spa@spa.com', '1234');\r\n"
-					+ "";
-			Statement statement = connection.createStatement();
-			statement.executeQuery(sql);
-		} catch (Exception e) {
-			e.getMessage();
-		} finally {
-			ConnectionPool.getInstance().returnConnection(connection);
-			System.out.println("created companies data");
-		}
-	}
-
-	public static void coupData() {
-		connection = null;
-		try {
-			connection = ConnectionPool.getInstance().getConnection();
-
-			String sql = "INSERT INTO `coupon_system`.`coupons` (`company_id`, `category_id`, `title`, `description`, `start_date`, `end_date`, `amount`, `price`, `image`) VALUES ('1', '1', 'food', 'free food', '2000-10-10', '2000-10-10', '50', '44', 'img1');\r\n"
-					+ "INSERT INTO `coupon_system`.`coupons` (`company_id`, `category_id`, `title`, `description`, `start_date`, `end_date`, `amount`, `price`, `image`) VALUES ('2', '2', 'electricity', 'free electricity', '2000-10-10', '2000-10-10', '43', '55', 'img2');\r\n"
-					+ "INSERT INTO `coupon_system`.`coupons` (`company_id`, `category_id`, `title`, `description`, `start_date`, `end_date`, `amount`, `price`, `image`) VALUES ('3', '3', 'restaurant', 'free burger', '2000-10-10', '2000-10-10', '21', '43', 'img3');\r\n"
-					+ "INSERT INTO `coupon_system`.`coupons` (`company_id`, `category_id`, `title`, `description`, `start_date`, `end_date`, `amount`, `price`, `image`) VALUES ('4', '4', 'vacation', 'free mani', '2000-10-10', '2000-10-10', '4', '22', 'img4');\r\n"
-					+ "";
-			Statement statement = connection.createStatement();
-			statement.executeQuery(sql);
-		} catch (Exception e) {
-			e.getMessage();
-		} finally {
-			ConnectionPool.getInstance().returnConnection(connection);
-			System.out.println("created coupon data");
-		}
-	}
 	
-	public static void custData() {
-		connection = null;
-		try {
-			connection = ConnectionPool.getInstance().getConnection();
 
-			String sql = "INSERT INTO `coupon_system`.`customers` (`first_name`, `last_name`, `email`, `password`) VALUES ('avi', 'cohen', 'avi@cohen.com', '1234');\r\n" + 
-					"INSERT INTO `coupon_system`.`customers` (`first_name`, `last_name`, `email`, `password`) VALUES ('ben', 'katz', 'ben@katz.com', '1234');\r\n" + 
-					"INSERT INTO `coupon_system`.`customers` (`first_name`, `last_name`, `email`, `password`) VALUES ('zohar', 'tor', 'zo@to.com', '1234');\r\n" + 
-					"";
-			Statement statement = connection.createStatement();
-			statement.executeQuery(sql);
-		} catch (Exception e) {
-			e.getMessage();
-		} finally {
-			ConnectionPool.getInstance().returnConnection(connection);
-			System.out.println("created coupon data");
-		}
-	}
-	
-	
 }
