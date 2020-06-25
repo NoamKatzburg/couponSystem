333c2f8474efbcd5fc142273e85a76527512f517
Noam Katzburg <noamkatzburg@gmail.com>
2020-06-25 12:30:28 +0300

    made methods to convert category to int and vice versa

--- src/noam/utils/MyUtils.java
+++ src/noam/utils/MyUtils.java
@@ -5,14 +5,46 @@ import noam.beans.Category;
 public class MyUtils {
 
 	public static java.sql.Date convertUtilToSql(java.util.Date uDate) {
-        java.sql.Date sDate = new java.sql.Date(uDate.getTime());
-        return sDate;
-    }
-	
+		java.sql.Date sDate = new java.sql.Date(uDate.getTime());
+		return sDate;
+	}
+
 	public static java.util.Date convertSqlToUtil(java.sql.Date sDate) {
-        java.util.Date uDate = new java.util.Date(sDate.getTime());
-        return uDate;
-    }
+		java.util.Date uDate = new java.util.Date(sDate.getTime());
+		return uDate;
+	}
+
+	public static  Category convertIntToCategory(int categoryId) {
+		switch (categoryId) {
+		case 1:
+			return Category.Food;
+		case 2:
+			return Category.Electricty;
+		case 3:
+			return Category.Restaurant;
+		case 4:
+			return Category.Vacation;
+		default:
+			break;
+		}
+		return null;
+	}
+
 	
-	
+	public static int convertStringToInt(String categoryName) {
+		switch (categoryName) {
+		case "Food":
+			return 0;
+		case "Electricty":
+			return 1;
+		case "Restaurant":
+			return 2;
+		case "Vacation":
+			return 3;
+		default:
+			break;
+		}
+		
+		return 0;
+	}
 }
