6389f6c4535d9273445655f525a45e640560be08
Noam Katzburg <noamkatzburg@gmail.com>
2020-06-27 23:00:18 +0300

    created new exceptions for Company bean

---
+++ src/noam/exceptions/CannotChangeIdException.java
@@ -0,0 +1,10 @@
+package noam.exceptions;
+
+public class CannotChangeIdException extends Exception {
+
+	public CannotChangeIdException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/noam/exceptions/CannotChangeNameException.java
@@ -0,0 +1,10 @@
+package noam.exceptions;
+
+public class CannotChangeNameException extends Exception {
+
+	public CannotChangeNameException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
